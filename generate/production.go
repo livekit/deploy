c875471bfa440d11f1b98bb9f65afd85770689c9
zehweh <cw@tf2.net>
2022-05-05 08:58:26 +0200

    fix domainRegexp
    domainRegexp currently rejects valid domain names.

--- generate/production.go
+++ generate/production.go
@@ -19,7 +19,7 @@ import (
 )
 
 var (
-	domainRegexp  = regexp.MustCompile(`^([A-Za-z0-9][A-Za-z0-9-]{1,62}[A-Za-z0-9]\.)+[A-Za-z]{2,6}$`)
+	domainRegexp  = regexp.MustCompile(`^(?:[_a-z0-9](?:[_a-z0-9-]{0,61}[a-z0-9])?\.)+(?:[a-z](?:[a-z0-9-]{0,61}[a-z0-9])?)?$`)
 	versionRegexp = regexp.MustCompile(`^v[0-9]+(\.[0-9]+){0,2}$`)
 )
 
